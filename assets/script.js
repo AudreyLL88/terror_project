7df8bfd225260baf10457342c80c889830a8a8c7
AudreyLL88 <a.lloancy@gmail.com>
2020-11-03 14:26:36 +0000

    Next button on result box

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,10 @@ const exit_btn = rule_box.querySelector(".buttons .quit");
 const continue_btn = rule_box.querySelector(".buttons .start");
 const quiz_box = document.querySelector(".quiz_box");
 const option_list = document.querySelector(".option_list");
-
+const timeCount = document.querySelector(".timer .timer_sec");
+const next_btn = quiz_box.querySelector(".next_btn");
+const result_box = document.querySelector(".result_box");
+const next_card = result_box.querySelector(".buttons .next_card");
 
 let activeCard = "None"
 
@@ -55,6 +58,7 @@ continue_btn.onclick =()=>{
     quiz_box.classList.add("activeQuiz");
      showQuestions(0);
      queCounter(1);
+     startTimer(15);
 }
 
 //Quiz box 
@@ -62,11 +66,11 @@ continue_btn.onclick =()=>{
 let que_count =  0;
 let que_numb = 1;
 let counter;
+let timeValue = 15;
 let userScore = 0;
 let totalScore = 0;
+let cardCount = 0;
 let questionsArray;
-
-const next_btn = quiz_box.querySelector(".next_btn");
 
 //Quizz box questions/answers from array in questions.js
 
@@ -100,6 +104,7 @@ function showQuestions(index){
 //Allow to select function and display correct/wrong answers
 
 function optionSelected(answer){
+    clearInterval(counter);
     let userAns = answer.textContent;
     let correctAns = questionsArray[que_count].answer;
     const allOptions = option_list.children.length;
@@ -129,14 +134,17 @@ function optionSelected(answer){
 //Next button display
 
 next_btn.onclick =()=>{
-   if(que_count < questions1.length -1){
+   if(que_count < questionsArray.length -1){
         que_count++;
         que_numb++;
     showQuestions(que_count);
     queCounter(que_numb);
+    clearInterval(counter);
+    startTimer(timeValue);
     next_btn.style.display = "none";
    }else{
        console.log("Questions completed");
+       showResult();
    };
 }
 
@@ -144,6 +152,61 @@ next_btn.onclick =()=>{
 
 function queCounter(index){
     const question_counter = document.querySelector(".total");
-    let totalQuestionCountTag = '<span><p>'+ index +'</p> of <p>'+ questions1.length +'</p> Questions</span>';
+    let totalQuestionCountTag = '<span><p>'+ index +'</p> of <p>'+ questionsArray.length +'</p> Questions</span>';
     question_counter.innerHTML = totalQuestionCountTag;  
+}
+
+//Timer
+
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time;
+        time--;
+        if(time < 9){
+            let addZero = timeCount.textContent;
+            timeCount.textContent = "0" + addZero;
+        }
+        if(time < 0){
+            clearInterval(counter);
+             timeCount.textContent = "00";
+        } 
+    }
+}
+
+// Card result box
+
+function showResult(){
+    console.log("total score " + totalScore);
+    cardCount += 1;
+    console.log("card count " + cardCount);
+    rule_box.classList.remove("activeInfo"); 
+    quiz_box.classList.remove("activeQuiz"); 
+    result_box.classList.add("activeResult"); 
+    const cardScore = result_box.querySelector(".card_score");
+    if (userScore > 3){ 
+        let scoreTag = '<span>and congrats! 🎉, You got <p>'+ userScore +'</p> out of <p>'+ questionsArray.length +'</p></span>';
+        cardScore.innerHTML = scoreTag;  
+    }
+    else if(userScore > 1){ 
+        let scoreTag = '<span>and congrats! 🎉, You got <p>'+ userScore +'</p> out of <p>'+ questionsArray.length +'</p></span>';
+        cardScore.innerHTML = scoreTag;
+    }
+    else{ 
+        let scoreTag = '<span>and congrats! 🎉, You got <p>'+ userScore +'</p> out of <p>'+ questionsArray.length +'</p></span>';
+        cardScore.innerHTML = scoreTag;
+    }
+}
+
+//Next button on result box
+
+next_card.onclick = ()=>{
+    result_box.classList.remove("activeResult"); //hide result box
+    timeValue = 15; 
+    que_count = 0;
+    que_numb = 1;
+    userScore = 0;
+    widthValue = 0;
+    container_disabled.classList.remove("disabled");
+    quiz_back.classList.remove("activeBack");
 }
