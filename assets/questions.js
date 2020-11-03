6e4ed12a30c23694a10a0195829ea9d4271f3720
AudreyLL88 <a.lloancy@gmail.com>
2020-11-03 12:11:08 +0000

    Creation of question array for quiz box

---
+++ assets/questions.js
@@ -0,0 +1,191 @@
+let questions1 = [
+    {
+    category: "Paranormal",
+    numb: 1,
+    question: "Where is the house located in Conjuring 1?",
+    answer: "Harrisville",
+    options: [
+      "Harrisville",
+      "Portland",
+      "Lexigton",
+      "San Francisco"
+    ]
+  },
+    {
+    category: "Paranormal",
+    numb: 2,
+    question: "What is the baby's name in Insidious 1?",
+    answer: "Cali",
+    options: [
+      "Cleo",
+      "Caroline",
+      "Cali",
+      "Anna"
+    ]
+  },
+    {
+    category: "Paranormal",
+    numb: 3,
+    question: "From which mythology does the demon in The exorcist comes from?",
+    answer: "The babylonian mythology",
+    options: [
+      "The egyptian mythology",
+      "The greek mythology",
+      "The nordic mythology",
+      "The babylonian mythology"
+    ]
+  },
+    {
+    category: "Paranormal",
+    numb: 4,
+    question: "Why is Rosemary pregnant with the devil in Rosemary's baby?",
+    answer: "For her husband's fame",
+    options: [
+      "For her husband's fame",
+      "She believes in Satan",
+      "She is part of a cult",
+      "It's not explained"
+    ]
+  },
+    {
+    category: "Paranormal",
+    numb: 5,
+    question: "What is the main evil object in Occulus?",
+    answer: "A mirror",
+    options: [
+      "A doll",
+      "A mirror",
+      "A necklace",
+      "A ring"
+    ]
+  }
+
+];
+
+let questions2 = [
+    {
+    category: "Slasher",
+    numb: 1,
+    question: "Who is the killer in The Nightmare on Elm Street ?",
+    answer: "Freddy Krueger",
+    options: [
+      "Freddy Krueger",
+      "Jason Voorhes",
+      "Leatherface",
+      "Michael Myers"
+    ]
+  },
+    {
+    category: "Slasher",
+    numb: 2,
+    question: "Where does Friday 13th take place?",
+    answer: "In Camp Crystal Lake",
+    options: [
+      "In Camp Crystal Lake",
+      "In Camp Green Lodge",
+      "In Camp Tree House",
+      "In Camp Night Owl"
+    ]
+  },
+    {
+    category: "Slasher",
+    numb: 3,
+    question: "Where does the opening scene in The Collection takes place?",
+    answer: "An underground nightclub",
+    options: [
+      "A suburb house",
+      "A swimming pool",
+      "A parking lot",
+      "An underground nightclub"
+    ]
+  },
+    {
+    category: "Slasher",
+    numb: 4,
+    question: "How does the final girl escape in Texas Chainsaw Massacre?",
+    answer: "Jumping in a pick-up truck",
+    options: [
+      "Running away",
+      "Saved by the police",
+      "Jumping in a pick-up truck",
+      "Swimming away"
+    ]
+  },
+    {
+    category: "Slasher",
+    numb: 5,
+    question: "In Scream, what does the killer ask on the phone?",
+    answer: "Do you like scary movies?",
+    options: [
+      "Knock knock?",
+      "Are you alone?",
+      "Do you like scary movies?",
+      "Can you see me?"
+    ]
+  }
+
+];
+
+let questions3 = [
+    {
+    category: "Monsters",
+    numb: 1,
+    question: "What is the name of the spaceship in Alien?",
+    answer: "Nostromo",
+    options: [
+      "Provolono",
+      "Nostromo",
+      "Sualoco",
+      "Bambino"
+    ]
+  },
+    {
+    category: "Monsters",
+    numb: 2,
+    question: "Where does Cloverfield takes place?",
+    answer: "New york",
+    options: [
+      "Boston",
+      "Los Angeles",
+      "Philadelphia",
+      "New york"
+    ]
+  },
+    {
+    category: "Monsters",
+    numb: 3,
+    question: "The earliest filmed version of Frankenstein was filmed in?",
+    answer: "1931",
+    options: [
+      "1931",
+      "1951",
+      "1941",
+      "1921"
+    ]
+  },
+    {
+    category: "Monsters",
+    numb: 4,
+    question: "The Tabonga appears in what monster film?",
+    answer: "From Hell it came",
+    options: [
+      "The Thing",
+      "From Hell it came",
+      "Evil Dead",
+      "Anaconda"
+    ]
+  },
+    {
+    category: "Monsters",
+    numb: 5,
+    question: "What is the setting for The Thing?",
+    answer: "Antartica",
+    options: [
+      "Greenland",
+      "Texas",
+      "Antartica",
+      "Ecuador"
+    ]
+  }
+
+];
